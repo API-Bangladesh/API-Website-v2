3b20a0990604e719936a6d0bbac978a10d4359d5
Jiaur Rhaman <107462642+jiaur83@users.noreply.github.com>
2024-02-01 15:03:47 +0600

    chat widget

--- components/Modules/ChatWidget/ChatWidget.js
+++ components/Modules/ChatWidget/ChatWidget.js
@@ -1,45 +1,70 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from 'next/link'
 import { TiMessages } from "react-icons/ti";
 import { LuMessagesSquare } from "react-icons/lu";
 import { IoCallOutline } from "react-icons/io5";
 import { LiaSmsSolid } from "react-icons/lia";
+import { MdWhatsapp } from "react-icons/md";
 
 const ChatWidget = () => {
-  const [isShown, setIsShown] = useState(false);
 
+  const [isShown, setIsShown] = useState(false);
   const handleClick = (event) => {
     setIsShown((current) => !current);
   };
 
+  // Loop through 3 items
+  const [currentItemIndex, setCurrentItemIndex] = useState(0);
+   useEffect(() => {
+     const interval = setInterval(() => {
+       setCurrentItemIndex((index) => (index + 1) % 3);
+     }, 3000); 
+ 
+     return () => clearInterval(interval);
+   }, []);
+   // Loop through 3 items
 
   return (
     <>
       <section>
           {isShown && (
               <div className="chatOptions">
-                <Link href="/contact-us" className="icons shadow mb-3" onClick={handleClick}>
+                <Link href="/contact-us" className="icons mb-3" onClick={handleClick}>
                   <LiaSmsSolid size={'22px'} className="text-white" />
                 </Link>
-                <Link href="tel:+8801686449007" className="icons shadow mb-3" onClick={handleClick}>
+                <Link href="tel:+8801686449007" className="icons mb-3" onClick={handleClick}>
                   <IoCallOutline size={'22px'} className="text-white" />
                 </Link>
-                <Link href="/" className="icons shadow mb-3" onClick={handleClick}>
-                  <LiaSmsSolid size={'22px'} className="text-white" />
-                </Link>
-
+                <a href="https://wa.me/8801551761805?text=Hello" target="_blank" className="icons mb-3" onClick={handleClick}>
+                  <MdWhatsapp size={'22px'} className="text-white" />
+                </a>
               </div>
             )}
 
         <div className="ChatBox">
             <button
               onClick={handleClick}
-              className="ChatBtn rounded-pill shadow border-0"
+              className="fadeOutBackground ChatBtn rounded-pill border-0"
             >
-              <LuMessagesSquare
-                size={"50px"}
-                color={"white"}
-              />
+              <div className={`item ${currentItemIndex === 0 ? "fadeIn" : "fadeOut"}`}>
+                <LuMessagesSquare
+                  size={"25px"}
+                  color={"white"}
+                />
+              </div>
+              <div className={`item ${currentItemIndex === 1 ? "fadeIn" : "fadeOut"}`}>
+              <IoCallOutline
+                  size={"25px"}
+                  color={"white"}
+                />
+              </div>
+              <div className={`item ${currentItemIndex === 2 ? "fadeIn" : "fadeOut"}`}>
+              <MdWhatsapp
+                  size={"25px"}
+                  color={"white"}
+                />
+              </div>
+
             </button>
         </div>
       </section>
