22c72c40e790c16641b1008cd8f9946fbb1b3797
Nazmul <nazmulhussain.pro@gmail.com>
2024-03-13 21:27:29 +0600

    Fix production

--- pages/Layout.js
+++ pages/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 import ChatWidget from "../components/Modules/ChatWidget/ChatWidget";
@@ -6,12 +7,18 @@ import ChatWidget from "../components/Modules/ChatWidget/ChatWidget";
 
  function Layout({children}) {
   return (
-    <>    
+    <>
+    <Head>
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+      />
+    </Head>
     <Header/>
     {children}
     <Footer/>
     <ChatWidget />
-    </> 
+    </>
   )
 }
 
